9d37677d0a3562ae8adf4423b83e99f1466528db
abhi <abhilashnaidu21@gmail.com>
2024-02-05 22:10:39 +0530

    new

--- Albertsons/src/test/java/BaseMethod/demo.java
+++ Albertsons/src/test/java/BaseMethod/demo.java
@@ -5,16 +5,18 @@ import static org.testng.Assert.assertEquals;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class demo {
-public static void main(String[] args) {
-	WebDriver driver = new ChromeDriver();
-	driver.get("http://localhost/opencart/upload/admin/");
-	driver.findElement(By.id("input-username")).sendKeys("admin");
-	driver.findElement(By.id("input-password")).sendKeys("admin");
-	driver.findElement(By.xpath("//button[@type='submit']")).click();
-	System.out.println(driver.getTitle());
-	assertEquals(driver.getTitle(), "Administration");
-	driver.close();
-}
+	@Test
+	public void test() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost/opencart/upload/admin/");
+		driver.findElement(By.id("input-username")).sendKeys("admin");
+		driver.findElement(By.id("input-password")).sendKeys("admin");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		System.out.println(driver.getTitle());
+		assertEquals(driver.getTitle(), "Administration");
+		driver.close();
+	}
 }
